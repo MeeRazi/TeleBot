1604963c01c83e71f07e04d1ef5a06d921befd9a
R A Z i <119275831+MeeRazi@users.noreply.github.com>
2024-08-24 14:32:53 +0530

    Update session.py

--- session.py
+++ session.py
@@ -1,9 +1,8 @@
 from pyrogram import Client
 
 print("Pyrogram V2 Session String Generator")
-
 API_ID = input("Enter API ID: ")
 API_HASH = input("Enter API HASH: ")
 
 with Client(name="USS", api_id=API_ID, api_hash=API_HASH, in_memory=True) as app:
-    print(app.export_session_string)
+    print(app.export_session_string())  # Add parentheses to call the method
